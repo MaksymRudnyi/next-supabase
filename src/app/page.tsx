fd3287a6054c8b6ac6d2cec78edb5872a0d089dd
Maksym Rudnyi <max.prog.zz@gmail.com>
2023-11-18 18:01:14 +0200

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default async function Home() {
   if (!session) {
     redirect('/login')
   }
-  const { data: tweets}  = await supabase.from('tweets').select();
+  const { data: tweets}  = await supabase.from('tweets').select('*, profiles(*)');
 
   console.log('data: ', tweets)
   return (
